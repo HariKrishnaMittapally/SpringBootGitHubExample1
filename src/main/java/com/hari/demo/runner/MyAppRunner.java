43757bb138283af5cb4477159ea5db5867dc8a08
Harikrishna <Harikrishna@192.168.100.7>
2020-09-27 11:14:35 +0530

    MYAPP RUNNER_IS MODIFIED WITH SLF4J

--- src/main/java/com/hari/demo/runner/MyAppRunner.java
+++ src/main/java/com/hari/demo/runner/MyAppRunner.java
@@ -1,15 +1,26 @@
 package com.hari.demo.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyAppRunner implements CommandLineRunner {
 
+	private static Logger log=LoggerFactory.getLogger(MyAppRunner.class);
+	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("WELCOME TO GITHUB");
-		System.out.println("Modified data");
+		int a=10;
+		try {
+			log.info("STARTED");
+			System.out.println("WELCOME TO GITHUB "+a);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("END OF FILE");
 	}
 
 }
